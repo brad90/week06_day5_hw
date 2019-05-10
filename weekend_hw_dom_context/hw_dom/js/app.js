75ad2a1bf2cfc09fa3c3b79aaf6d8e7eb6d94aff
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-05-10 14:59:38 +0100

    Finished Basic Code

---
+++ weekend_hw_dom_context/hw_dom/js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const newFormInfo = document.querySelector('#form');
+  newFormInfo.addEventListener('submit', handleFormInfoSubmit)
+
+  console.log('Javascript loaded')
+
+});
+
+
+const handleFormInfoSubmit = function(event){
+
+  event.preventDefault();
+
+  const listItem = document.createElement('li');
+
+  const listH1 = document.createElement('h1');
+  const listH3 = document.createElement('h3');
+  const listP = document.createElement('p');
+
+
+  listH1.textContent = `${event.target.shareName.value}`
+  listH3.textContent = `${event.target.marketCap.value}`
+  listP.textContent = `${event.target.select.value}`
+
+  listItem.appendChild(listH1)
+  listItem.appendChild(listH3)
+  listItem.appendChild(listP)
+
+  const pageList = document.querySelector('#share_list')
+  pageList.appendChild(listItem)
+}
